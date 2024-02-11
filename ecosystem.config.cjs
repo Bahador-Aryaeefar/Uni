5482a4ec32ae623c9d78a1f9ed35534acf553fd3
Bahador-Aryaeefar <bahadoraryaeefar@gmail.com>
2024-02-11 23:47:47 +0330

    ss

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,11 @@
 module.exports = {
-    apps: [
-      {
-        name: 'NuxtAppName',
-        exec_mode: 'cluster',
-        instances: 'max', // Or a number of instances
-        script: './node_modules/nuxt/bin/nuxt.js',
-        args: 'start'
-      }
-    ]
-  }
+  apps: [
+    {
+      name: 'NuxtAppName',
+      port: '3000',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './.output/server/index.mjs'
+    }
+  ]
+}
