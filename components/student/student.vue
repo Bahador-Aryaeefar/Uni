0f7b03297c385ed19e11fd3a18e0e6db51b4c1ec
Bahador Aryaeefar <bahadoraryaeefar@gmail.com>
2024-02-28 16:50:31 +0330

    animation

--- components/student/student.vue
+++ components/student/student.vue
@@ -4,7 +4,8 @@
             <div>
                 <div class="flex justify-between gap-5 items-center">
                     <span class="text-[#757575] font-bold text-xl leading-[2rem]">نام درس</span>
-                    <div class="shrink-0 p-2 text-[#757575] text-xs rounded-[0.25rem] border-[0.125rem] border-[#EEEEEE]">گروه ۰۱
+                    <div class="shrink-0 p-2 text-[#757575] text-xs rounded-[0.25rem] border-[0.125rem] border-[#EEEEEE]">
+                        گروه ۰۱
                     </div>
                 </div>
 
@@ -19,5 +20,117 @@
                 <div class="mt-3 font-bold text-sm text-[#9E9E9E]">وضعیت حضور و غیاب</div>
             </div>
         </div>
+
+        <div class="text-[#212121] font-bold text-[2rem] mt-[3.75rem] text-center">سر کلاس حاضر هستید؟</div>
+
+        <div class="mt-[3.875rem] w-[16rem] h-[16rem] relative mx-auto">
+            <div class="absolute w-full h-full bg-[#4CAF50] opacity-[0.4] circle1"></div>
+            <div class="absolute w-full h-full bg-[#4CAF50] opacity-[0.4] circle2"></div>
+            <div class="absolute w-full h-full bg-[#4CAF50] opacity-[0.4] circle3"></div>
+            <div
+                class="absolute w-full h-full bg-[#4CAF50] rounded-full flex items-center justify-center text-white  text-xl font-bold text-white">
+                بله؛ ثبت حضور
+            </div>
+        </div>
     </div>
 </template>
+
+<style scoped>
+.circle1 {
+    /* transform-origin: 50%-10% 50%-20%; */
+    animation: spin1 8s linear infinite;
+    scale: 1.14;
+}
+
+.circle2 {
+    /* transform-origin: 50%-10% 50%-20%; */
+    animation: spin2 8s linear infinite;
+    scale: 1.12;
+}
+
+.circle3 {
+    /* transform-origin: 50%-10% 50%-20%; */
+    animation: spin3 8s linear infinite;
+    scale: 1.1;
+}
+
+@keyframes spin1 {
+    0% {
+        border-radius: 60% 75% 65% 80%;
+    }
+
+    25% {
+        border-radius: 85% 90% 70% 125%;
+        rotate: 90deg;
+    }
+
+    50% {
+        border-radius: 80% 85% 95% 70%;
+        rotate: 180deg;
+    }
+
+    75% {
+        border-radius: 75% 95% 80% 85%;
+        rotate: 270deg;
+    }
+
+    100% {
+        border-radius: 60% 75% 65% 80%;
+        rotate: 360deg;
+    }
+}
+
+@keyframes spin2 {
+    0% {
+        border-radius: 60% 75% 65% 80%;
+        rotate: 270deg;
+    }
+
+    25% {
+        border-radius: 85% 90% 70% 125%;
+        rotate: 360deg;
+    }
+
+    50% {
+        border-radius: 80% 85% 95% 70%;
+        rotate: 90deg;
+    }
+
+    75% {
+        border-radius: 75% 95% 80% 85%;
+        rotate: 180deg;
+    }
+
+    100% {
+        border-radius: 60% 75% 65% 80%;
+        rotate: 270deg;
+    }
+}
+
+@keyframes spin3 {
+    0% {
+        border-radius: 60% 75% 65% 80%;
+        rotate: 90deg;
+    }
+
+    25% {
+        border-radius: 85% 90% 70% 125%;
+        rotate: 180deg;
+    }
+
+    50% {
+        border-radius: 80% 85% 95% 70%;
+        rotate: 270deg;
+    }
+
+    75% {
+        border-radius: 75% 95% 80% 85%;
+        rotate: 360deg;
+    }
+
+    100% {
+        border-radius: 60% 75% 65% 80%;
+        rotate: 90deg;
+    }
+}
+</style>
